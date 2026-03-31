fbe9419b02b343507aabbd0d6a76f17841d4562d
Constanza Campos <constanza.campos7@virginiogomez.cl>
2026-03-30 21:41:08 -0300

    subiendo Ejercicio 2 Módulo 7

---
+++ app.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const express = require('express');
+const db = require('./db');
+
+const app = express();
+app.use(express.json())
+
+app.get('/usuarios', async (req, res) => {
+  try {
+    const result = await db.query('SELECT * FROM usuarios ORDER BY fecha DESC');
+    res.json(result.rows) //rows y fields son propiedades del objeto result que devuelve el método query de pg, rows contiene los datos y fields la información de las columnas
+  } catch (error) {
+    res.status(500).json(error);
+  }
+})
+
+app.get('/usuarios/:fecha', async (req, res) => {
+  const { fecha } = req.params
+  console.log(fecha);
+  try {
+    const result = await db.query(
+  `SELECT * 
+   FROM usuarios 
+   WHERE fecha @> ARRAY[$1::timestamp]
+   ORDER BY fecha DESC`,
+  [fecha] // El parámetro puede ser solo la fecha, PG le pondrá 00:00:00
+);
+    console.log(result);
+    res.json(result.rows)
+  } catch (error) {
+    res.status(500).json(error);
+  }
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`Servidor en puerto http://localhost:${PORT}/usuarios`));
---
+++ db.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const { Pool } = require('pg')
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+});
+
+async function query(text, params) {
+  const res = await pool.query(text, params)
+  return res
+}
+
+module.exports = { query }
